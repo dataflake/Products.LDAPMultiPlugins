d74265e79f55d8b5a5ed81103dec2d960636097c
Jens Vagelpohl <jens@plyp.com>
2023-02-02 17:36:22 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(name):
 
 
 setup(name='Products.LDAPMultiPlugins',
-      version='5.0',
+      version='5.1.dev0',
       description='LDAP-backed plugins for the Zope PluggableAuthService',
       long_description=read('README.rst'),
       classifiers=[
